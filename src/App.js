ad1182564d1067fd196a3fe756cad00bf9ef45b6
wangziqi0503 <493375503@qq.com>
2019-06-19 18:51:13 +0800

    完成项目

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Header from './common/header';
 import store from './store';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Detail from './pages/detail';
+import Detail from './pages/detail/loadable.js';
 import Login from './pages/login';
+import Write from './pages/write';
 
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
                 <Route path='/' exact component={Home}></Route>
                 <Route path='/login' exact component={Login}></Route>
                 <Route path='/detail/:id' exact component={Detail}></Route>
+                <Route path='/write' exact component={Write}></Route>
               </div>
             </BrowserRouter>
             <GlobalStyle />
---
+++ src/pages/write/index.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom'
+// import { actionCreators } from './store';
+
+class Write extends PureComponent {
+    render() {
+        const { loginStatus } = this.props;
+
+        if( loginStatus ){
+            return (
+                <div>写文章</div>
+             )
+        } else {
+            return <Redirect to='/login' />
+        }
+       
+    }
+}
+
+const mapState = (state) => ({
+    loginStatus: state.getIn(['login', 'login'])
+})
+
+
+export default connect(mapState, null)(Write);
